e0e2519a146b7d13b191820ad4e1e06c344a8f4d
cbzhang <zcbinvip@gmail.com>
2023-08-10 20:12:14 +0800

    init

---
+++ src/main/java/com/example/jenkinsdemo/test/Demo.java
@@ -0,0 +1,19 @@
+package com.example.jenkinsdemo.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Zhang Chengbin
+ * @date: 2023/8/10
+ * @Copyright：Copyright ® 掌控网络. All rights reserved.
+ */
+@RestController
+@RequestMapping("/demo")
+public class Demo {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String hello) {
+        return "hello:" + hello;
+    }
+}
